6d1ace459f89e6d09a9774e488fb81485eb4cc41
pashawildenauer <44805488+pashawildenauer@users.noreply.github.com>
2019-11-07 21:59:09 +0800

    Add files via upload

--- fmi.js
+++ fmi.js
@@ -1,3 +1,14 @@
+$(function() {
+    var leftDrawer = $('.left-nav-drawer');
+    $('#menu-logo').click(function() {
+        leftDrawer.addClass('open');
+    });
+
+    $('.close-block>span').click(function(){
+        leftDrawer.removeClass('open');
+    });
+});
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
