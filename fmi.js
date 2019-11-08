0ef7aa0aa7d75640c3bedd3bb98f94b3924353e3
pashawildenauer <44805488+pashawildenauer@users.noreply.github.com>
2019-11-09 00:28:33 +0800

    Add files via upload

--- fmi.js
+++ fmi.js
@@ -1,3 +1,25 @@
+$(window).load(function(){
+            setTimeout(function(){
+                $('#preloader').velocity({
+                    opacity : 0.1,
+                    translateY: "-80px"
+                }, {
+                    duration: 400,
+                    complete: function(){
+                    $('#hola').velocity({
+                    translateY : "-100%"
+                }, {
+                    duration: 1000,
+                    easing: [0.7,0,0.3,1],
+                    complete: function(){
+                        $('.home').addClass('animate-border divide');
+                    }
+                })  
+                    }
+                })
+            },1000)
+        })
+
 $(function() {
     var leftDrawer = $('.left-nav-drawer');
     $('#menu-logo').click(function() {
@@ -18,3 +40,19 @@ function myFunction() {
     x.className = "topnav";
   }
 } 
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
